ca5e026f67a167bd031b31e7839e9ed175364bc1
hardikankoliya07 <hardik.a@webcodegenie.com>
2023-05-16 12:14:23 +0530

    create ui for chat module

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const Chat = require('../models/chat');
+const User = require('../models/user');
 
 router.get('/', async (req, res, next) => {
-    res.render('chat/index', { title: 'Chats' })
+    const data = await User.find({ _id: { $ne: req.user._id } }, { _id: 1, full_name: 1, profile: 1 }).lean()
+    res.render('chat/index', { title: 'Chats', data: data })
 })
 
 module.exports = router;
